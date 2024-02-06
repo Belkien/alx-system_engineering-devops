f6b4804e263fcb05a5bd7798361ca14303c4eb9a
Belkien <66989368+Belkien@users.noreply.github.com>
2024-02-05 19:46:02 -0800

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Function querying subscribers on a given Reddit API."""
+"""A function that queries the Reddit API and
+returns the number of subscribers"""
 import requests
-import sys
+
 
 def number_of_subscribers(subreddit):
-    """  Args:
-        subreddit: subreddit name
-    Returns:
-        number of subscribers to the subreddit,
-        or 0 if subreddit requested NOT VALID"""
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) 9'}
+    """returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
-
-
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,23 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API and
+prints the titles of the first 10 hot posts
+listed for a given subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'api advanced 0x16 (by/Belkien)'}  
-
-    response = requests.get(url, headers=headers)
-
-    """ Check if the request was successful """
-    if response.status_code == 200:
-        data = response.json()
-
-        """Checking if the subreddit exists"""
-        if 'error' in data:
-            print(f"Error: {data['message']}")
-            return
-
-        """Extracts and print titles of the first 10 hot posts"""
-        for i, post in enumerate(data['data']['children'][:10], start=1):
-            print(f"{i}. {post['data']['title']}")
-
-    else:
-        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
-
-
+    """prints the titles of the first 10 hot posts listed"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
